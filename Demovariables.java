eeef645d6fb75e4841f98c7563638327cb8a3c3f
Manikantareddy777 <karamellasai2006@gmail.com>
2026-01-20 17:33:39 +0530

    Add files via upload

---
+++ Demovariables.java
@@ -0,0 +1,13 @@
+class Demovariables
+{
+	public static void main(String[] args)
+	{
+		int age;//variable declaration
+		age= 21;//variable intialization
+		String name="mani";//variable declaration & intialization
+		System.out.println(age);
+		System.out.println(name);
+		
+	}
+}
+
